46e26b3a09a5bdfc9cc9238782b9cd936c7a4580
AlexUngureanu <a.ungureanu0099@gmail.com>
2023-10-22 19:13:15 +0300

    Added first implementation of the LUT pixel color change

--- Framework/ViewModel/Commands/SplineToolsMenuCommands.cs
+++ Framework/ViewModel/Commands/SplineToolsMenuCommands.cs
@@ -1,4 +1,6 @@
-﻿using Framework.Converters;
+﻿using Emgu.CV.Structure;
+using Emgu.CV;
+using Framework.Converters;
 using Framework.Utilities;
 using System;
 using System.Collections.Generic;
@@ -7,7 +9,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace Framework.ViewModel.Commands
 {
@@ -55,23 +56,30 @@ namespace Framework.ViewModel.Commands
 
             var canvases = (object[])parameters;
 
-            DataProvider.SplineToolVectorOfMousePosition.Clear();
-            DataProvider.SplineToolVectorOfMousePosition.Add(new Point(0, _splineToolVM.Graph.Height));
+            var controlPoints = new List<Point>
+            {
+                new Point(0, _splineToolVM.Graph.Height)
+            };
             for (int i = Math.Max(DataProvider.VectorOfMousePosition.Count - 5, 0); i < DataProvider.VectorOfMousePosition.Count; i++)
             {
-                DataProvider.SplineToolVectorOfMousePosition.Add(DataProvider.VectorOfMousePosition[i]);
+                controlPoints.Add(DataProvider.VectorOfMousePosition[i]);
             }
-            DataProvider.SplineToolVectorOfMousePosition.Add(new Point(_splineToolVM.Graph.Width, 0));
+            controlPoints.Add(new Point(_splineToolVM.Graph.Width, 0));
 
-            var points = DataProvider.SplineToolVectorOfMousePosition.OrderBy(point => point.X).Select(point => new Point(point.X, _splineToolVM.Graph.Height - point.Y)).ToList();
+            var points = controlPoints.OrderBy(point => point.X).Select(point => new Point(point.X, _splineToolVM.Graph.Height - point.Y)).ToList();
             points.Add(new Point(points[points.Count - 1].X - (points[points.Count - 2].X - points[points.Count - 1].X), points[points.Count - 2].Y));
             points.Add(new Point(-points[1].X, points[1].Y));
             points = points.OrderBy(point => point.X).ToList();
 
-            List<Point> curve = GenerateCubicHermiteSplinePoints(points);
-            curve = curve.Select(point => new Point(point.X, _splineToolVM.Graph.Height - point.Y)).ToList();
+            List<Point> curve = GenerateCubicHermiteSplinePoints(points)
+                .Select(point => new Point(point.X, _splineToolVM.Graph.Height - point.Y))
+                .Where(point => point.X >= 0 && point.X <= _splineToolVM.Graph.Width)
+                .Select(point => new Point(point.X, point.Y < 0 ? 0 : point.Y))
+                .ToList();
 
-            UiHelper.DrawSplineToolCurve(canvases[0] as Canvas,canvases[1] as Canvas, curve, ScaleValue, Brushes.Blue);
+            DataProvider.SplineToolCurvePoints = new System.Windows.Media.PointCollection(curve);
+
+            UiHelper.DrawSplineToolCurve(canvases[0] as Canvas, canvases[1] as Canvas, curve, ScaleValue, Brushes.Blue);
         }
 
         public List<Point> GenerateCubicHermiteSplinePoints(List<Point> points)
@@ -109,6 +117,89 @@ namespace Framework.ViewModel.Commands
 
         #endregion
 
+        #region Update Gray Values
+
+        private ICommand _updateGrayValuesCommand;
+        public ICommand UpdateGrayValuesCommand
+        {
+            get
+            {
+                if (_updateGrayValuesCommand == null)
+                    _updateGrayValuesCommand = new RelayCommand(UpdateGrayValues);
+                return _updateGrayValuesCommand;
+            }
+        }
+
+        private void UpdateGrayValues(object parameters)
+        {
+            var lutValues = new Dictionary<double, double>();
+            try
+            {
+                var curvePoints = GetCurvePointsAtSinglePixel();
+                lutValues = curvePoints
+                    .GroupBy(point => NormalizeValue(point.X, _splineToolVM.OriginalCanvasWidth))
+                    .ToDictionary(group => group.Key, group => NormalizeValue(_splineToolVM.OriginalCanvasHeight - group.Last().Y, _splineToolVM.OriginalCanvasHeight));
+
+                var image = DataProvider.ColorInitialImage.Clone();
+                for (int y = 0; y < image.Height; y++)
+                {
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        byte grayValue = image.Data[y, x, 0];
+                        byte lutRawValue = (byte)lutValues[grayValue];
+
+                        byte lutValue = (byte)(255 - lutRawValue);
+                        image.Data[y, x, 0] = lutValue;
+                        image.Data[y, x, 1] = lutValue;
+                        image.Data[y, x, 2] = lutValue;
+                    }
+                }
+                Graph = ImageConverter.Convert(image);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                //MessageBox.Show(lutValues.Count.ToString());
+            }
+        }
+
+        private List<Point> GetCurvePointsAtSinglePixel()
+        {
+            List<Point> result = new List<Point>();
+            for (double x = 0; x <= _splineToolVM.ProcessedGraph.Width; x += 1)
+            {
+                Point point = GetCurvePointAtX(x);
+                result.Add(point);
+            }
+            return result;
+        }
+
+        private Point GetCurvePointAtX(double x)
+        {
+            List<Point> points = DataProvider.SplineToolCurvePoints.OrderBy(point => point.X).ToList();
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                if (points[i].X <= x && points[i + 1].X >= x)
+                {
+                    double t = (x - points[i].X) / (points[i + 1].X - points[i].X);
+                    double y = (1 - t) * points[i].Y + t * points[i + 1].Y;
+                    return new Point(x, y);
+                }
+            }
+            return new Point(x, 0);
+        }
+
+        private double NormalizeValue(double value, double size)
+        {
+            var normalizedValue = value / size * 255;
+            return Math.Max(0, Math.Min(255, Math.Round(normalizedValue)));
+        }
+
+        #endregion
+
         #region Reset
 
         private ICommand _resetCommand;
@@ -125,6 +216,10 @@ namespace Framework.ViewModel.Commands
         private void Reset(object parameters)
         {
             var canvases = (object[])parameters;
+
+            Image<Bgr, byte> image = new Image<Bgr, byte>((int)_splineToolVM.OriginalCanvasWidth, (int)_splineToolVM.OriginalCanvasHeight, new Bgr(255, 255, 255));
+
+            Graph = ImageConverter.Convert(image);
 
             DrawingHelper.RemoveUiElements(canvases[0] as Canvas);
             DrawingHelper.RemoveUiElements(canvases[1] as Canvas);
