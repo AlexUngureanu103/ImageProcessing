02a7a5051424f188b4abac7ff61d8e8c946c6c24
Alexandru Ungureanu <a.ungureanu0099@gmail.com>
2023-12-17 13:28:40 +0200

    updated the spherical deformation

--- Algorithms/Sections/GeometricTransformations.cs
+++ Algorithms/Sections/GeometricTransformations.cs
@@ -14,7 +14,7 @@ namespace Algorithms.Sections
             var x0 = image.Width / 2;
             var y0 = image.Height / 2;
 
-            Parallel.For(0, img.Height, y =>
+            for (int y = 0; y < img.Height; y++)
             {
                 for (int x = 0; x < img.Width; x++)
                 {
@@ -22,43 +22,45 @@ namespace Algorithms.Sections
                     var dy = y - y0;
                     var r = Math.Sqrt(dx * dx + dy * dy);
 
-                    if (r >= rMax)
+                    if (r <= rMax)
                     {
-                        continue;
+                        var z = Math.Sqrt(rMax * rMax - r * r);
+                        var dxSqrt = Math.Sqrt(dx * dx + z * z);
+                        var dySqrt = Math.Sqrt(dy * dy + z * z);
+                        var betaX = (1 - 1 / reflectionIndex) * Math.Asin(dx / dxSqrt);
+                        var betaY = (1 - 1 / reflectionIndex) * Math.Asin(dy / dySqrt);
+
+                        var xInverse = Math.Max(0, x - z * Math.Tan(betaX));
+                        var yInverse = Math.Max(0, y - z * Math.Tan(betaY));
+
+                        var x1 = (int)Math.Floor(xInverse);
+                        var y1 = (int)Math.Floor(yInverse);
+                        var x2 = x1 + 1;
+                        var y2 = y1 + 1;
+
+                        var weightX2 = xInverse - x1;
+                        var weightX1 = 1 - weightX2;
+                        var weightY2 = yInverse - y1;
+                        var weightY1 = 1 - weightY2;
+
+                        x1 = Math.Max(0, Math.Min(x1, image.Width - 1));
+                        y1 = Math.Max(0, Math.Min(y1, image.Height - 1));
+                        x2 = Math.Max(0, Math.Min(x2, image.Width - 1));
+                        y2 = Math.Max(0, Math.Min(y2, image.Height - 1));
+
+                        var pixelValue = weightX1 * weightY1 * image.Data[y1, x1, 0] +
+                                         weightX2 * weightY1 * image.Data[y1, x2, 0] +
+                                         weightX1 * weightY2 * image.Data[y2, x1, 0] +
+                                         weightX2 * weightY2 * image.Data[y2, x2, 0];
+
+                        img.Data[y, x, 0] = (byte)pixelValue;
                     }
-
-                    var z = Math.Sqrt(rMax * rMax - r * r);
-                    var dxSqrt = Math.Sqrt(dx * dx + z * z);
-                    var dySqrt = Math.Sqrt(dy * dy + z * z);
-                    var betaX = (1 - 1 / reflectionIndex) * Math.Asin(dx / dxSqrt);
-                    var betaY = (1 - 1 / reflectionIndex) * Math.Asin(dy / dySqrt);
-
-                    var xInverse = Math.Max(0, x0 - z * Math.Tan(betaX));
-                    var yInverse = Math.Max(0, y0 - z * Math.Tan(betaY));
-
-                    var x1 = (int)Math.Floor(xInverse);
-                    var y1 = (int)Math.Floor(yInverse);
-                    var x2 = x1 + 1;
-                    var y2 = y1 + 1;
-
-                    var weightX2 = xInverse - x1;
-                    var weightX1 = 1 - weightX2;
-                    var weightY2 = yInverse - y1;
-                    var weightY1 = 1 - weightY2;
-
-                    x1 = Math.Max(0, Math.Min(x1, image.Width - 1));
-                    y1 = Math.Max(0, Math.Min(y1, image.Height - 1));
-                    x2 = Math.Max(0, Math.Min(x2, image.Width - 1));
-                    y2 = Math.Max(0, Math.Min(y2, image.Height - 1));
-
-                    var pixelValue = weightX1 * weightY1 * image.Data[y1, x1, 0] +
-                                     weightX2 * weightY1 * image.Data[y1, x2, 0] +
-                                     weightX1 * weightY2 * image.Data[y2, x1, 0] +
-                                     weightX2 * weightY2 * image.Data[y2, x2, 0];
-
-                    img.Data[y, x, 0] = (byte)pixelValue;
+                    else
+                    {
+                        img.Data[y, x, 0] = image.Data[y, x, 0];
+                    }
                 }
-            });
+            };
 
             return img;
         }
@@ -78,26 +80,28 @@ namespace Algorithms.Sections
                     var dy = y - y0;
                     var r = Math.Sqrt(dx * dx + dy * dy);
 
-                    if (r >= rMax)
+                    if (r <= rMax)
                     {
-                        continue;
+                        var z = Math.Sqrt(rMax * rMax - r * r);
+
+                        var dxSqrt = Math.Sqrt(dx * dx + z * z);
+                        var betaX = (1 - 1 / refractionIndex) * Math.Asin(dx / dxSqrt);
+
+                        var dySqrt = Math.Sqrt(dy * dy + z * z);
+                        var betaY = (1 - 1 / refractionIndex) * Math.Asin(dy / dySqrt);
+
+                        var xInverse = Math.Max(0, x - z * Math.Tan(betaX));
+                        var yInverse = Math.Max(0, y - z * Math.Tan(betaY));
+
+                        xInverse = Math.Min(xInverse, image.Width - 1);
+                        yInverse = Math.Min(yInverse, image.Height - 1);
+
+                        pixelData[y, x] = (byte)BiliniarInterpolation(image, xInverse, yInverse);
                     }
-
-                    var z = Math.Sqrt(rMax * rMax - r * r);
-
-                    var dxSqrt = Math.Sqrt(dx * dx + z * z);
-                    var betaX = (1 - 1 / refractionIndex) * Math.Asin(dx / dxSqrt);
-
-                    var dySqrt = Math.Sqrt(dy * dy + z * z);
-                    var betaY = (1 - 1 / refractionIndex) * Math.Asin(dy / dySqrt);
-
-                    var xInverse = Math.Max(0, x0 - z * Math.Tan(betaX));
-                    var yInverse = Math.Max(0, y0 - z * Math.Tan(betaY));
-
-                    xInverse = Math.Min(xInverse, image.Width - 1);
-                    yInverse = Math.Min(yInverse, image.Height - 1);
-
-                    pixelData[y, x] = (byte)BiliniarInterpolation(image, xInverse, yInverse);
+                    else
+                    {
+                        pixelData[y, x] = image.Data[y, x, 0];
+                    }
                 }
             });
 
